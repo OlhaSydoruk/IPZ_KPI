95d45ae4d4e8b3210889e51e7d14f2b47afba5b4
OlhaSydoruk <sidorukolgasev@gmail.com>
2024-12-25 21:43:54 +0100

    code does not work

--- src/test/java/com/example/authorarticlesproject/AuthorArticlesProjectApplicationTests.java
+++ src/test/java/com/example/authorarticlesproject/AuthorArticlesProjectApplicationTests.java
@@ -2,12 +2,23 @@ package com.example.authorarticlesproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
 
 @SpringBootTest
 class AuthorArticlesProjectApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @MockBean
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @MockBean
+    private RedisConnectionFactory connectionFactory;
+
+    @MockBean
+    private ReactiveRedisConnectionFactory reactiveRedisConnectionFactory;
+
 
 }
